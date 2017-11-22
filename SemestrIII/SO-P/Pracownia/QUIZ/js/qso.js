0b9c3a276ca3845d88dbd29a52e8cfb6b8b4af22
mbronek7 <mbronek7@gmail.com>
2017-11-23 00:51:27 +0100

    Stan na 23.11.17

---
+++ SemestrIII/SO-P/Pracownia/QUIZ/js/qso.js
@@ -0,0 +1,83 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.currentQuestionIndex = 0;
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+    this.currentQuestionIndex++;
+};
+
+Quiz.prototype.getCurrentQuestion = function() {
+    return this.questions[this.currentQuestionIndex];
+};
+
+Quiz.prototype.hasEnded = function() {
+    return this.currentQuestionIndex >= this.questions.length;
+};
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function (choice) {
+    return this.answer === choice;
+};
+var QuizUI = {
+    displayNext: function () {
+        if (quiz.hasEnded()) {
+            this.displayScore();
+        } else {
+            this.displayQuestion();
+            this.displayChoices();
+            this.displayProgress();
+        }
+    },
+    displayQuestion: function() {
+        this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
+    },
+    displayChoices: function() {
+        var choices = quiz.getCurrentQuestion().choices;
+
+        for(var i = 0; i < choices.length; i++) {
+            this.populateIdWithHTML("choice" + i, choices[i]);
+            this.guessHandler("guess" + i, choices[i]);
+        }
+    },
+    displayScore: function() {
+        var gameOverHTML = "<center><h1>Koniec</h1></center>";
+        gameOverHTML += "<center><h2> Twój wynik: " + quiz.score + "/" + quiz.questions.length + "</h2></center>"+"  <center>  <footer> Autor testu: Michał Bronikowski</footer> </center>";
+        this.populateIdWithHTML("quiz", gameOverHTML);
+    },
+
+    populateIdWithHTML: function(id, text) {
+        var element = document.getElementById(id);
+        element.innerHTML = text;
+    },
+    guessHandler: function(id, guess) {
+        var button = document.getElementById(id);
+        button.onclick = function() {
+            quiz.guess(guess);
+            QuizUI.displayNext();
+        }
+    },
+
+    displayProgress: function() {
+        var currentQuestionNumber = quiz.currentQuestionIndex + 1;
+        this.populateIdWithHTML("progress", "Pytanie " + currentQuestionNumber + " z " + quiz.questions.length);
+    }
+};
+//Create Questions
+var questions = [
+    new Question("Co nie pasuje do reszty?", ["UNIX","LINUX", "PREPROCESOR", "MS-DOS"], "PREPROCESOR") // stawiaj przecinki i lec dalej
+];
+
+//Create Quiz
+var quiz = new Quiz(questions);
+
+//Display Quiz
+QuizUI.displayNext();
