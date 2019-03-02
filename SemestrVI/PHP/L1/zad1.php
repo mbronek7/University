a81aceb35685a8f01148c5935ffa5b2ddd43f1bb
mbronek7 <mbronek7@gmail.com>
2019-03-02 17:00:40 +0100

    Stan na 02.03.2019

---
+++ SemestrVI/PHP/L1/zad1.php
@@ -0,0 +1,19 @@
+<?php
+
+  $letters = $argv[1];
+  $substitutions = $argv[2];
+  $sentence = $argv[3];
+  $result = $argv[3];
+  $letters_size = strlen($letters);
+  $sentence_size = strlen($sentence);
+
+  for($i = 0; $i < $letters_size; $i++) {
+    $temp = str_replace($letters[$i], $substitutions[$i], $sentence);
+    for ($x = 0; $x < $sentence_size; $x++) {
+      if($sentence[$x] != $temp[$x]){
+        $result[$x] = $temp[$x];
+      }
+    }
+  }
+  echo $result;
+?>
